011f2ab2681b9ef3f66b64ac54515fdd2671211c
Ellian Campos <ellian.campos12@gmail.com>
2025-01-03 17:55:50 -0600

    Changes

--- PRESENTATION/SalesFrm.Designer.cs
+++ PRESENTATION/SalesFrm.Designer.cs
@@ -29,14 +29,6 @@
         private void InitializeComponent()
         {
             dgvSales = new DataGridView();
-            comboBox3 = new ComboBox();
-            comboBox2 = new ComboBox();
-            comboBox1 = new ComboBox();
-            label6 = new Label();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
             panel1 = new Panel();
             lblRafleName = new Label();
             label1 = new Label();
@@ -55,97 +47,9 @@
             dgvSales.Location = new Point(12, 62);
             dgvSales.Name = "dgvSales";
             dgvSales.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvSales.Size = new Size(776, 347);
+            dgvSales.Size = new Size(776, 376);
             dgvSales.TabIndex = 19;
             dgvSales.CellDoubleClick += dgvSales_CellDoubleClick;
-            // 
-            // comboBox3
-            // 
-            comboBox3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            comboBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Items.AddRange(new object[] { "5", "10", "20", "50" });
-            comboBox3.Location = new Point(547, 415);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(58, 29);
-            comboBox3.TabIndex = 18;
-            // 
-            // comboBox2
-            // 
-            comboBox2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            comboBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Items.AddRange(new object[] { "5", "10", "20", "50" });
-            comboBox2.Location = new Point(644, 415);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(58, 29);
-            comboBox2.TabIndex = 17;
-            // 
-            // comboBox1
-            // 
-            comboBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            comboBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "5", "10", "20", "50" });
-            comboBox1.Location = new Point(83, 415);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(58, 29);
-            comboBox1.TabIndex = 16;
-            // 
-            // label6
-            // 
-            label6.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label6.Location = new Point(708, 418);
-            label6.Name = "label6";
-            label6.Size = new Size(80, 21);
-            label6.TabIndex = 15;
-            label6.Text = "(50) items";
-            // 
-            // label5
-            // 
-            label5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(611, 418);
-            label5.Name = "label5";
-            label5.Size = new Size(27, 21);
-            label5.TabIndex = 14;
-            label5.Text = "de";
-            // 
-            // label4
-            // 
-            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(485, 418);
-            label4.Name = "label4";
-            label4.Size = new Size(56, 21);
-            label4.TabIndex = 13;
-            label4.Text = "Página";
-            // 
-            // label3
-            // 
-            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(147, 418);
-            label3.Name = "label3";
-            label3.Size = new Size(85, 21);
-            label3.TabIndex = 12;
-            label3.Text = "por página";
-            // 
-            // label2
-            // 
-            label2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(12, 418);
-            label2.Name = "label2";
-            label2.Size = new Size(65, 21);
-            label2.TabIndex = 11;
-            label2.Text = "Mostrar";
             // 
             // panel1
             // 
@@ -211,14 +115,6 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(dgvSales);
-            Controls.Add(comboBox3);
-            Controls.Add(comboBox2);
-            Controls.Add(comboBox1);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(panel1);
             Name = "SalesFrm";
             Text = "Rifas a la Tica";
@@ -228,20 +124,11 @@
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvSales;
-        private ComboBox comboBox3;
-        private ComboBox comboBox2;
-        private ComboBox comboBox1;
-        private Label label6;
-        private Label label5;
-        private Label label4;
-        private Label label3;
-        private Label label2;
         private Panel panel1;
         private Label label1;
         private Button btnAddRaffle;
